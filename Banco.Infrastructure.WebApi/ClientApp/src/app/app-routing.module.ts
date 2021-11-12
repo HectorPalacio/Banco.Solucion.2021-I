8ba42a88bc29d3285bb8df0f242bc9478a6bdf48
HectorPalacio <palaciorivas@gmail.com>
2021-11-12 15:48:38 -0500

    Creación del módulo crearCuenta

--- Banco.Infrastructure.WebApi/ClientApp/src/app/app-routing.module.ts
+++ Banco.Infrastructure.WebApi/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearCuentaComponent } from './cuentaBancaria/crear-cuenta/crear-cuenta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'crearCuenta', component: CrearCuentaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
