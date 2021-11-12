a5fb206abffd35df7032abb493806f2e499eed93
HectorPalacio <palaciorivas@gmail.com>
2021-11-12 16:45:47 -0500

    Módulos home, nav-menu e implementación de Bootstrap

--- Banco.Infrastructure.WebApi/ClientApp/src/app/app-routing.module.ts
+++ Banco.Infrastructure.WebApi/ClientApp/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrearCuentaComponent } from './cuentaBancaria/crear-cuenta/crear-cuenta.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: 'crearCuenta', component: CrearCuentaComponent
+  },
+  {
+    path: '', component: HomeComponent
   }
 ];
 
--- Banco.Infrastructure.WebApi/ClientApp/src/app/app.module.ts
+++ Banco.Infrastructure.WebApi/ClientApp/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrearCuentaComponent } from './cuentaBancaria/crear-cuenta/crear-cuenta.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CrearCuentaComponent,
+    NavMenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
